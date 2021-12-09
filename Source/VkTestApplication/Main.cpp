2ce9c63b9597adc1423836a7f60db94ecb8c3e2a
Lawrence <Lawrence.martin.green@gmail.com>
2021-12-09 22:29:40 +0000

    Enumerate instance layers

--- Source/VkTestApplication/Main.cpp
+++ Source/VkTestApplication/Main.cpp
@@ -1,6 +1,22 @@
-
+#include "vulkan.h"
+#include <vector>
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
-
+	std::cout << "==================================================" << std::endl;
+	std::cout << "================= Starting Test ==================" << std::endl;
+	std::cout << "==================================================" << std::endl;
+	// Enumerate the Vulkan instance layers
+	uint32_t layerCount;
+	vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+	std::vector<VkLayerProperties> layers(layerCount);
+	vkEnumerateInstanceLayerProperties(&layerCount, layers.data());
+	
+	std::cout << "Found the following layers :" << std::endl;
+	for (const VkLayerProperties& layer : layers)
+	{
+		std::cout << "\t* " << layer.layerName << std::endl;
+	}
+	
 }
