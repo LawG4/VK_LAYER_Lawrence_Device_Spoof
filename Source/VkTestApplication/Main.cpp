7b4df412a3e5ba045b1f508c2c9bf8f4e9e7ed6c
Lawrence <Lawrence.martin.green@gmail.com>
2021-12-10 00:09:39 +0000

    Put applications runtime directory into contents of VK_LAYER_PATH

--- Source/VkTestApplication/Main.cpp
+++ Source/VkTestApplication/Main.cpp
@@ -1,9 +1,20 @@
 #include "vulkan.h"
+
+#include <filesystem>
 #include <vector>
 #include <iostream>
+#include <stdlib.h>
 
 int main(int argc, char* argv[])
 {
+	std::string currentDirectory = std::filesystem::current_path().string();
+	std::cout << "Using the runtime directory as : \n" << currentDirectory << "\n\n" << std::endl;
+
+	// Place this directory as the VK_LAYER_PATH environment variable
+	std::string envCommand = "VK_LAYER_PATH=" + currentDirectory;
+	putenv(envCommand.c_str());
+
+	// It's not guarenteed, but use arg 1 as the working directory for the binary
 	std::cout << "==================================================" << std::endl;
 	std::cout << "================= Starting Test ==================" << std::endl;
 	std::cout << "==================================================" << std::endl;
