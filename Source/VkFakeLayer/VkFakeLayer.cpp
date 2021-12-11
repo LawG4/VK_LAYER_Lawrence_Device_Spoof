6cacb3534d79364f0b580a612a9ffb10c173f9cb
Lawrence <Lawrence.martin.green@gmail.com>
2021-12-11 23:40:34 +0000

    Expose vkNegotiateLoaderLayerInterfaceVersion

--- Source/VkFakeLayer/VkFakeLayer.cpp
+++ Source/VkFakeLayer/VkFakeLayer.cpp
@@ -5,3 +5,20 @@ VK_LAYER_EXPORT VKAPI_ATTR PFN_vkVoidFunction VKAPI_CALL vkGetInstanceProcAddr(V
 {
     return nullptr;
 }
+
+VK_LAYER_EXPORT VKAPI_ATTR VkResult VKAPI_CALL
+vkNegotiateLoaderLayerInterfaceVersion(VkNegotiateLayerInterface* pVersionStruct)
+{
+    // How we want to tell the Vulkan loader how our layer works
+    // First ensure we've recieved the right struct
+    if (!pVersionStruct) return VK_ERROR_INITIALIZATION_FAILED;
+    if (pVersionStruct->sType != LAYER_NEGOTIATE_INTERFACE_STRUCT) return VK_ERROR_INITIALIZATION_FAILED;
+
+    // What version of the struct is the loader using?
+    if (pVersionStruct->loaderLayerInterfaceVersion >= 2) {
+        pVersionStruct->pfnGetInstanceProcAddr = &vkGetInstanceProcAddr;
+    }
+
+    // Got to the end okay
+    return VK_SUCCESS;
+}
